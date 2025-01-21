876c518ba81bb9848393a0718d8269ae37803421
d.fedorenko <dqniejl@gmail.com>
2025-01-22 00:44:12 +0200

    initial commit

---
+++ src/main/java/com/hiregami/analyze_service/HiregamiAnalyzeServiceApplication.java
@@ -0,0 +1,13 @@
+package com.hiregami.analyze_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HiregamiAnalyzeServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HiregamiAnalyzeServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hiregami/analyze_service/HiregamiAnalyzeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hiregami.analyze_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HiregamiAnalyzeServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
